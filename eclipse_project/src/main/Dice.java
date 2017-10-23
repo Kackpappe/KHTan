2ba8036155537f999ed86bff181689e51a529e07
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-23 19:12:02 +0200

    MESSAGE

---
+++ eclipse_project/src/main/Dice.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Dice {
+	    public int rolled;
+		public int rolled_dice1;
+		public int rolled_dice2;
+		public int rolled_dice_total;
+
+		public int getRolled() {
+			return this.rolled;
+		}
+		
+	}
+
