684144101b600d1747b082587c5796cf6a610e25
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-23 18:58:48 +0200

    1

---
+++ eclipse_project/src/main/GameTile.java
@@ -0,0 +1,7 @@
+package main;
+
+public class GameTile {
+	private enum type { Siedlung, Straße, Stadt, Räuber }
+	private enum color { Rot, Grün, Orange, Blau, Schwarz }
+	private int winpoints;
+}
