24676e97c0e000b63260b6074b01e285c6098d7f
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-24 14:06:14 +0200

    Testklasse

--- eclipse_project/src/main/KaisTestKlasse.java
+++ eclipse_project/src/main/KaisTestKlasse.java
@@ -4,8 +4,7 @@ public class KaisTestKlasse {
 
 		private static main.Dice Dice;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+				
 		Dice = new main.Dice();
 		Dice = Dice.roll();
 		System.out.print("Erster Würfel = " + Dice.rolled_dice1);
