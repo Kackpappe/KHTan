d831a8b4e6b8d270502a4e476951fee755d7c864
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-24 14:12:22 +0200

    Hafen - Getter&Setter

--- eclipse_project/src/main/Harbour.java
+++ eclipse_project/src/main/Harbour.java
@@ -2,7 +2,26 @@ package main;
 
 public class Harbour {
 	private int geben;
-	private int nehmen; 	// idR immer 1
+	private int nehmen = 1; 	// Immer 1
 	private main.ResourceType GivenResource;
+	public int getGeben() {
+		return geben;
+	}
+	public void setGeben(int geben) {
+		this.geben = geben;
+	}
+	public int getNehmen() {
+		return nehmen;
+	}
+	public void setNehmen(int nehmen) {
+		this.nehmen = nehmen;
+	}
+	public main.ResourceType getGivenResource() {
+		return GivenResource;
+	}
+	public void setGivenResource(main.ResourceType givenResource) {
+		GivenResource = givenResource;
+	}
 
+	
 }
