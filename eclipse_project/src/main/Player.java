73b338cd7139775fa5c90f732127ed6f9800d04c
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-24 15:37:14 +0200

    Player CardType ==> Type

--- eclipse_project/src/main/Player.java
+++ eclipse_project/src/main/Player.java
@@ -43,13 +43,13 @@ public class Player {
 		public void setWinpooints(int winpooints) {
 			Winpooints = winpooints;
 		}
-		public ResourceCard gatherResourceCard(ResourceCard card) {
+		public ResourceCard gatherResourceCard(ResourceType type) {
 			//TODO Karte (zum bezhahlen) aus Stock entnehmen
 			ResourceCard resCard = null;
 			ResourceCard returnCard = null;
 			for(int i = 0; i < ResourceCardList.size(); i++) {
 				resCard = ResourceCardList.get(i);
-				if(resCard.getResourceType() == card.getResourceType()) {
+				if(resCard.getResourceType() == type) {
 					returnCard = resCard;
 					ResourceCardList.remove(i);
 				}
