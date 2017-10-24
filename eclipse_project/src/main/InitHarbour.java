ff0f60bdd6a8dc31c1fa60b861933ae8dae05832
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-24 14:35:27 +0200

    Hafen-Initiator

---
+++ eclipse_project/src/main/InitHarbour.java
@@ -0,0 +1,20 @@
+package main;
+
+public class InitHarbour {
+	// Alle Häfen isntanziieren
+	// Die Anzahl der Häfen basiert auf einer Tabelle
+	// LOOP Tabelle
+	
+	public void InitHarbour() {
+		Harbour hafen = new Harbour();
+		hafen.setGeben(3); //Zahl wird aus Tabelle gelesen
+		hafen.setNehmen(1);
+		ResourceType givenResource = ResourceType.Holz; //Typ wird aus Tabelle gelesen
+		hafen.setGivenResource(givenResource);
+	}
+	
+	
+	
+	
+
+}
