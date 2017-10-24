d93244522ad51609025c710bd8255074926aa791
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-24 15:10:41 +0200

    Player AddResourceCard

--- eclipse_project/src/main/Player.java
+++ eclipse_project/src/main/Player.java
@@ -1,12 +1,19 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Player {
 		private String Name;
 		private main.Color Color;
 		private int Winpooints;
-		private main.ResourceCard ResourceCards;
+		private ArrayList<ResourceCard> ResourceCardList;
 		//private main.   Entwicklunskarte
 		private main.SpecialCardType KnightlyPower;
 		private main.SpecialCardType TradingPower;
 		//private main.   Bustatus
+
+		public void AddResourceCard(ResourceCard card) {
+			ResourceCardList.add(card);
+		}
+
 }
