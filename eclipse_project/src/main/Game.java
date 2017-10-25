acbc2d24482ce9a4dcff24b1e93729eaabc054d2
cH33@D250 <cH33@D250@192.168.0.187>
2017-10-25 23:33:20 +0200

    Game + Test

--- eclipse_project/src/main/Game.java
+++ eclipse_project/src/main/Game.java
@@ -11,25 +11,21 @@ public class Game {
 		public void main(ArrayList<Player> player) {
 			//Spielfeld und Karten initialisieren
 			InitialSequence init = new InitialSequence();
-			Bank bank = init.main(playerList);
+			Bank bank = init.main(player);
 			
-			for(int i = 0; player.get(i).getWinpoints() < 10; i++) {
-				
-			}
-			
-			
-			
-			
-			// 1. Würfeln
-			Dice dice = new Dice().roll();
-			int diceTotal = dice.rolled_dice_total;
-			// 2. Rohstoffe verteilen oder Räuber
-			if (diceTotal == 7) {
-				// Räuber!
-			}
-			else {
-				// Prüfen, welche Spieler betroffen sind
-			}
+			// Spielzug
+			for(int i = 0; i < player.size(); i++) {
+									
+				// 1. Würfeln
+				Dice dice = new Dice().roll();
+				// 2. Rohstoffe verteilen oder Räuber
+				if (dice.rolled_dice_total == 7) {
+					// Räuber!
+				}
+				else {
+					// Prüfen, welche Spieler betroffen sind
+				}
 	
+		}
 		}
 }
--- eclipse_project/src/main/TestGame.java
+++ eclipse_project/src/main/TestGame.java
@@ -3,48 +3,37 @@ package main;
 import java.util.ArrayList;
 
 public class TestGame {
-		private static Player player = new Player();
 		private static Bank bank = new Bank();
 		private static ResourceCard resCard;
 		private static DevelopmentCard devCard;
 		private static SpecialCard specCard;
 		
 		public static void main(String[] args) {
-			ArrayList<ResourceCard> ResList = new ArrayList<ResourceCard>();
-			ArrayList<DevelopmentCard> DevList = new ArrayList<DevelopmentCard>();
-			ArrayList<SpecialCard> SpecList = new ArrayList<SpecialCard>();
-			
-			resCard = new ResourceCard(ResourceType.Holz);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Holz);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Getreide);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Getreide);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Erz);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Erz);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Wolle);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Wolle);
-			ResList.add(resCard);
-			resCard = new ResourceCard(ResourceType.Lehm);
-			ResList.add(resCard);
-			
-			devCard = new Knight(1);			
-			DevList.add(devCard);
-			devCard = new Building(BuildingType.Parlament, 1);
-			DevList.add(devCard);
-			
-			specCard = new SpecialCard();
-			SpecList.add(specCard);
-			
-			bank.fillStock(ResList, DevList, SpecList);
-			
-			player.setName("Testspieler");
+			ArrayList<Player> playerList = new ArrayList<Player>();
+			Player player = new Player();
+			player.setName("Testspieler 1");
 			player.setColor(Color.Rot);
+			playerList.add(player);
+			
+			player = new Player();
+			player.setName("Testspieler 2");
+			player.setColor(Color.Blau);
+			playerList.add(player);
+			
+			Game game = new Game();
+			game.main(playerList);
+			
+			
+			System.out.println(playerList.get(0).getName());
+			System.out.println(playerList.get(0).getColor());
+			System.out.println(playerList.get(0).getWinpoints());
+			System.out.println("");
+			System.out.println(playerList.get(1).getName());
+			System.out.println(playerList.get(1).getColor());
+			System.out.println(playerList.get(1).getWinpoints());
+			
+			
+			
 						
 		}
 }
