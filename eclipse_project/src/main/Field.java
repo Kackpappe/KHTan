7be692ad09e702d45d188fda8bd6098ecbbc155e
frank <frank@192.168.2.11>
2017-10-30 09:35:44 +0100

    Initialisierung Spielfeld

--- eclipse_project/src/main/Field.java
+++ eclipse_project/src/main/Field.java
@@ -1,144 +1,38 @@
 package main;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
-
-import javax.xml.parsers.*;
-
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 
 public class Field {
 	
 	private HashMap<Integer, Hexagon> hexagons;
-	
-	
-	
-	
-//	private ArrayList<Coordinate> coordinates;
-//	private HashMap<Integer, Crossing> crossings;
+	private HashMap<Integer, Crossing> crossings;
+	private InitField initField;
 	
 	
 	public Field() {
 		hexagons = new HashMap<Integer, Hexagon>();
+		crossings = new HashMap<Integer, Crossing>();
+		initField = new InitField(this);
 	}
 	
 	
 	public void init() {
-		Document doc = null;
-		
-		try {
-			doc = loadXML("src/main/Spielfeld.xml");
-			doc.getDocumentElement().normalize();
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		NodeList hexList = doc.getElementsByTagName("hex");
-		
-		for (int i = 0; i < hexList.getLength(); i++) {
-			Element hexElement = (Element) hexList.item(i);
-			
-			Hexagon hexagon = createHexagon(hexElement);
-			hexagons.put(hexagon.getId(), hexagon);
-			
-			HashMap<HexagonNeighbor, Integer> neighbors = getNeighbors(hexElement);
-			
-			for (HexagonNeighbor hexNeighbor : neighbors.keySet()) {
-				Integer hexId = neighbors.get(hexNeighbor);
-				
-				if (hexId != null) {
-					Hexagon neighbor = hexagons.get(hexId);
-					setNeighbor(hexagon, neighbor, hexNeighbor);
-				}
-			}
-		}
+		initField.initialIzeFierld();
 	}
 	
 	
-	private Document loadXML(String fileName) throws ParserConfigurationException, SAXException, IOException {
-		File fXmlFile = new File(fileName);
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		return dBuilder.parse(fXmlFile);
+	public HashMap<Integer, Hexagon> getHexagons() {
+		return hexagons;
 	}
 	
 	
-	private Hexagon createHexagon(Element hexNode) {
-		String id = hexNode.getAttribute("id");
-		String type = hexNode.getAttribute("type");
-		String number = hexNode.getAttribute("number");
-		
-		HexagonType hexType = null;
-		
-		switch (type) {
-			case "0": { hexType = HexagonType.Wasser; break; }
-			case "1": { hexType = HexagonType.Wolle; break; }
-			case "2": { hexType = HexagonType.Lehm; break; }
-			case "3": { hexType = HexagonType.Holz; break; }
-			case "4": { hexType = HexagonType.Getreide; break; }
-			case "5": { hexType = HexagonType.Erz; break; }
-			case "6": { hexType = HexagonType.Wueste; break; }
-		}
-		
-		return new Hexagon(Integer.valueOf(id), hexType, Integer.valueOf(number));
+	public HashMap<Integer, Crossing> getCrossings() {
+		return crossings;
 	}
-	
-	
-	private HashMap<HexagonNeighbor, Integer> getNeighbors(Element hexElement) {
-		HashMap<HexagonNeighbor, Integer> neighbors = new HashMap<HexagonNeighbor, Integer>();
-		
-		Element neighborsElement = (Element) hexElement.getElementsByTagName("neighbors").item(0);
-		NodeList neighborList = neighborsElement.getElementsByTagName("neighbor");
-		
-		for (int i = 0; i < neighborList.getLength(); i++) {
-			Element neighborElement = (Element) neighborList.item(i);
-			
-			String number = neighborElement.getAttribute("number");
-			String id = neighborElement.getAttribute("id");
-			
-			Integer neighborId = null;
-			HexagonNeighbor hexNeighbor = null;
-			
-			if (!id.equals("")) {
-				neighborId = Integer.valueOf(id);
-			}
-			
-			switch (number) {
-				case "1": { 	hexNeighbor = HexagonNeighbor.neighbor1; break; }
-				case "2": { 	hexNeighbor = HexagonNeighbor.neighbor2; break; }
-				case "3": { 	hexNeighbor = HexagonNeighbor.neighbor3; break; }
-				case "4": { 	hexNeighbor = HexagonNeighbor.neighbor4; break; }
-				case "5": { 	hexNeighbor = HexagonNeighbor.neighbor5; break; }
-				case "6": { 	hexNeighbor = HexagonNeighbor.neighbor6; break; }
-			}
-			
-			neighbors.put(hexNeighbor, neighborId);
-		}
-		
-		return neighbors;
-	}
-	
-	
-	private void setNeighbor(Hexagon hexagon, Hexagon neighbor, HexagonNeighbor hexNeighbor) {
-		HexagonNeighbor hn = null;
-		
-		switch (hexNeighbor) {
-			case neighbor1: { hn = HexagonNeighbor.neighbor4; break; } 
-			case neighbor2: { hn = HexagonNeighbor.neighbor5; break; }
-			case neighbor3: { hn = HexagonNeighbor.neighbor6; break; }
-			case neighbor4: { hn = HexagonNeighbor.neighbor1; break; }
-			case neighbor5: { hn = HexagonNeighbor.neighbor2; break; }
-			case neighbor6: { hn = HexagonNeighbor.neighbor3; break; }
-		}
-		
-		hexagon.getNeighbors().put(hexNeighbor, neighbor);
-		
-		if (neighbor != null) {
-			neighbor.getNeighbors().put(hn, hexagon);
-		}
+
+
+	public InitField getInitField() {
+		return initField;
 	}
 	
 }
--- eclipse_project/src/main/InitCrossing.java
+++ eclipse_project/src/main/InitCrossing.java
@@ -1,60 +1,48 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class InitCrossing {
 	
 	private Hexagon hexagon;
+	private ArrayList<CrossingNeighbors> neighbors;
 	
 	public InitCrossing(Hexagon hexagon) {
 		this.hexagon = hexagon;
+		
+		neighbors = new ArrayList<CrossingNeighbors>();
+		neighbors.add(new CrossingNeighbors(HexagonCorners.corner1, HexagonNeighbor.neighbor6, HexagonNeighbor.neighbor1));
+		neighbors.add(new CrossingNeighbors(HexagonCorners.corner2, HexagonNeighbor.neighbor1, HexagonNeighbor.neighbor2));
+		neighbors.add(new CrossingNeighbors(HexagonCorners.corner3, HexagonNeighbor.neighbor2, HexagonNeighbor.neighbor3));
+		neighbors.add(new CrossingNeighbors(HexagonCorners.corner4, HexagonNeighbor.neighbor3, HexagonNeighbor.neighbor4));
+		neighbors.add(new CrossingNeighbors(HexagonCorners.corner5, HexagonNeighbor.neighbor4, HexagonNeighbor.neighbor5));
+		neighbors.add(new CrossingNeighbors(HexagonCorners.corner6, HexagonNeighbor.neighbor5, HexagonNeighbor.neighbor6));
 	}
 	
 	
-	public void initCrossings() {
-		HashMap<HexagonCorners, Crossing> crossings = hexagon.getCrossings();
+	public void initializeCrossings() {
 		CrossingProvider provider = CrossingProvider.getInstance();
 		
-		if (hexagon.getType() == HexagonType.Wasser) {
-			
-		} else {
-			ArrayList<CrossingNeighbors> neighbors = new ArrayList<CrossingNeighbors>();
-			neighbors.add(new CrossingNeighbors(HexagonCorners.corner1, HexagonNeighbor.neighbor6, HexagonNeighbor.neighbor1));
-			neighbors.add(new CrossingNeighbors(HexagonCorners.corner2, HexagonNeighbor.neighbor1, HexagonNeighbor.neighbor2));
-			neighbors.add(new CrossingNeighbors(HexagonCorners.corner3, HexagonNeighbor.neighbor2, HexagonNeighbor.neighbor3));
-			neighbors.add(new CrossingNeighbors(HexagonCorners.corner4, HexagonNeighbor.neighbor3, HexagonNeighbor.neighbor4));
-			neighbors.add(new CrossingNeighbors(HexagonCorners.corner5, HexagonNeighbor.neighbor4, HexagonNeighbor.neighbor5));
-			neighbors.add(new CrossingNeighbors(HexagonCorners.corner6, HexagonNeighbor.neighbor5, HexagonNeighbor.neighbor6));
-			
-			for (CrossingNeighbors cn : neighbors) {
-				Crossing crossing = hexagon.getCrossings().get(cn.corner);
+		for (CrossingNeighbors cn : neighbors) {
+			if (hexagon.getType() == HexagonType.Wasser) {
+				boolean neighborsOk = checkNeighbors(cn.neighbor1, cn.neighbor2);
 				
-				if (crossing == null) {
-					crossing = provider.provideCrossing();
-					hexagon.getCrossings().put(cn.corner, crossing);
+				if (neighborsOk == false) {
+					continue;
 				}
-				
-				crossing.getHexagons().put(hexagon.getId(), hexagon);
-				
-				Hexagon neighbor = hexagon.getNeighbors().get(cn.neighbor1);
 			}
 			
+			Crossing crossing = hexagon.getCrossings().get(cn.corner);
 			
-//			Crossing crossing1 = provider.provideCrossing();
-//			crossing1.getHexagons().add(hexagon);
-			
-			Hexagon neighbor6 = hexagon.getNeighbors().get(HexagonNeighbor.neighbor6);
-			
-			if (neighbor6 != null) {
-				Crossing crossing = neighbor6.getCrossings().get(HexagonCorners.corner3);
-				
-				if (crossing == null) {
-					crossing = provider.provideCrossing();
-				}
-				
-				
+			if (crossing == null) {
+				crossing = provider.provideCrossing();
+				hexagon.getCrossings().put(cn.corner, crossing);
 			}
+			
+			crossing.getHexagons().put(hexagon.getId(), hexagon);
+			
+			assignHexagonAndCorner(cn.corner, cn.neighbor1, crossing);
+			assignHexagonAndCorner(cn.corner, cn.neighbor2, crossing);
 		}
 	}
 	
@@ -113,6 +101,37 @@ public class InitCrossing {
 				return null;
 			}
 		}
+	}
+	
+	
+	private void assignHexagonAndCorner(HexagonCorners corner, HexagonNeighbor neighbor, Crossing crossing) {
+		Hexagon hexNeighbor = hexagon.getNeighbors().get(neighbor);
+		HexagonCorners neighborCorner = getHexagonCornerOfNeighbor(neighbor, corner);
+		Crossing neighborCrossing = hexNeighbor.getCrossings().get(neighborCorner);
+		
+		if (neighborCrossing == null) {
+			crossing.getHexagons().put(hexNeighbor.getId(), hexNeighbor);
+			hexNeighbor.getCrossings().put(neighborCorner, crossing);
+		} else {
+			neighborCrossing.getHexagons().put(hexagon.getId(), hexagon);
+			hexagon.getCrossings().put(corner, neighborCrossing);
+		}
+	}
+	
+	
+	private boolean checkNeighbors(HexagonNeighbor neighbor1, HexagonNeighbor neighbor2) {
+		Hexagon hexNeighbor1 = hexagon.getNeighbors().get(neighbor1);
+		Hexagon hexNeighbor2 = hexagon.getNeighbors().get(neighbor2);
+		
+		if ((hexNeighbor1 == null) || (hexNeighbor2 == null)) {
+			return false;
+		}
+		
+		if ((hexNeighbor1.getType() == HexagonType.Wasser) && (hexNeighbor2.getType() == HexagonType.Wasser)) {
+			return false;
+		}
+		
+		return true;
 	}
 	
 	
